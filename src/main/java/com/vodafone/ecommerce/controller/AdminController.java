880f84b15a8840a54b00492eaa4d5e08ba7c1c7c
Shehab Kotb <shehab.kotb@vodafone.com>
2023-06-07 20:39:27 +0300

    add ui prototype with basic authentication

---
+++ src/main/java/com/vodafone/ecommerce/controller/AdminController.java
@@ -0,0 +1,18 @@
+package com.vodafone.ecommerce.controller;
+
+
+import com.vodafone.ecommerce.dto.ProductDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class AdminController {
+
+    @GetMapping(value = {"/admin", "admin/dashboard"})
+    public String adminDashboard(Model model) {
+        return "admin-dashboard";
+    }
+}
---
+++ src/main/java/com/vodafone/ecommerce/security/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,35 @@
+package com.vodafone.ecommerce.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
+
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    SimpleUrlAuthenticationSuccessHandler userSuccessHandler =
+            new SimpleUrlAuthenticationSuccessHandler("/products");
+    SimpleUrlAuthenticationSuccessHandler adminSuccessHandler =
+            new SimpleUrlAuthenticationSuccessHandler("/admin");
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        for (final GrantedAuthority grantedAuthority : authorities) {
+            String authorityName = grantedAuthority.getAuthority();
+            if (authorityName.equals("ROLE_ADMIN")) {
+                // if the user is an ADMIN delegate to the adminSuccessHandler
+                this.adminSuccessHandler.onAuthenticationSuccess(request, response, authentication);
+                return;
+            }
+        }
+        // if the user is not an admin delegate to the userSuccessHandler
+        this.userSuccessHandler.onAuthenticationSuccess(request, response, authentication);
+    }
+}
+
