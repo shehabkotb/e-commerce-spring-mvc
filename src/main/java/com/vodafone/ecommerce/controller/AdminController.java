634adb8914026052306c7cfbe1801ef3c8d40d14
Muhammad Bakr <mbakr7757@gmail.com>
2023-06-08 17:53:27 +0300

    add endpoint for adding new product

--- src/main/java/com/vodafone/ecommerce/controller/AdminController.java
+++ src/main/java/com/vodafone/ecommerce/controller/AdminController.java
@@ -2,28 +2,47 @@ package com.vodafone.ecommerce.controller;
 
 
 import com.vodafone.ecommerce.dto.ProductDto;
+import com.vodafone.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @Controller
 public class AdminController {
 
-    @GetMapping(value = {"/admin", "admin/dashboard"})
+    @Autowired
+    ProductService productService;
+
+    @GetMapping(value = {"/admin", "/admin/dashboard"})
     public String adminDashboard(Model model) {
         return "admin-dashboard";
     }
 
-    @GetMapping("admin/users")
+    @GetMapping("/admin/users")
     public String adminUsersManagement(Model model) {
         return "admin-users";
     }
 
-    @GetMapping("admin/products")
+    @GetMapping("/admin/products")
     public String adminProductManagement(Model model) {
+        model.addAttribute("products", productService.getAllProducts());
         return "admin-products";
     }
 
+    @GetMapping("/admin/products/add")
+    public String adminAddProduct(Model model) {
+        return "admin-addProduct";
+    }
+
+    @PostMapping("/admin/products/add")
+    public String adminSaveProduct(@ModelAttribute("product") ProductDto productDto, BindingResult bindingResult, Model model) {
+        productService.saveProduct(productDto);
+        return "redirect:/admin/products";
+    }
 }
