9b96471ec9d5930306de98df614d733bf8df4943
hema <ibrahemkhaled1907@gmail.com>
2023-06-09 16:54:44 +0300

    Update findByRole in UserRepo

--- src/main/java/com/vodafone/ecommerce/repository/UserRepository.java
+++ src/main/java/com/vodafone/ecommerce/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.vodafone.ecommerce.repository;
 
+import com.vodafone.ecommerce.enums.Role;
 import com.vodafone.ecommerce.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
@@ -11,6 +14,9 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findByEmailIgnoreCase(String email);
 
     Boolean existsByEmail(String email);
+    List<UserEntity> findByRole(Role role);
+
+
 
 
 }
