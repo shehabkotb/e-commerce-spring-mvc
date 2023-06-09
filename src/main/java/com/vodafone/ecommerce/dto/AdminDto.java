f12d625cc5668a6ae81dea434d3310ec330c6405
hema <ibrahemkhaled1907@gmail.com>
2023-06-09 20:23:06 +0300

    Modify for AdminDto

--- src/main/java/com/vodafone/ecommerce/dto/AdminDto.java
+++ src/main/java/com/vodafone/ecommerce/dto/AdminDto.java
@@ -1,17 +1,14 @@
 package com.vodafone.ecommerce.dto;
 
-import com.vodafone.ecommerce.enums.Category;
 import com.vodafone.ecommerce.enums.Role;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
-public class UserEntityDto {
+public class AdminDto {
     private Long id;
     @NotBlank(message = "userName Should not be Empty")
     private String username;
--- src/main/java/com/vodafone/ecommerce/mapper/AdminMapper.java
+++ src/main/java/com/vodafone/ecommerce/mapper/AdminMapper.java
@@ -1,28 +1,30 @@
 package com.vodafone.ecommerce.mapper;
 
-import com.vodafone.ecommerce.dto.UserEntityDto;
+import com.vodafone.ecommerce.dto.AdminDto;
 import com.vodafone.ecommerce.model.UserEntity;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @AllArgsConstructor
-public class UserMapper {
+public class AdminMapper {
     private final PasswordEncoder passwordEncoder;
 
 
-    public static UserEntity mapToUser(UserEntityDto userEntityDto) {
+    public static UserEntity mapToAdmin(AdminDto adminDto) {
 
 
         return UserEntity.builder()
-                .username(userEntityDto.getUsername())
-                .email(userEntityDto.getEmail())
-                .role(userEntityDto.getRole())
-                .password(userEntityDto.getPassword())
+                .id(adminDto.getId())
+                .username(adminDto.getUsername())
+                .email(adminDto.getEmail())
+                .role(adminDto.getRole())
+                .password(adminDto.getPassword())
                 .build();
     }
 
-    public static UserEntityDto mapToUserDto(UserEntity user) {
-        return UserEntityDto.builder()
+    public static AdminDto mapToAdminDto(UserEntity user) {
+        return AdminDto.builder()
+                .id(user.getId())
                 .username(user.getUsername())
                 .email(user.getEmail())
                 .role(user.getRole())
