fc15ff1fba162d5e1aeb8d3fe65890a3aed2a451
Muhammad Bakr <mbakr7757@gmail.com>
2023-06-10 14:11:56 +0300

    add getCurrentUserName method to return the username from the session

--- src/main/java/com/vodafone/ecommerce/controller/CartController.java
+++ src/main/java/com/vodafone/ecommerce/controller/CartController.java
@@ -1,8 +1,10 @@
 package com.vodafone.ecommerce.controller;
 
+import com.vodafone.ecommerce.security.SecurityUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CartController {
@@ -10,4 +12,9 @@ public class CartController {
     public String getCart(Model model) {
         return "customer-cart";
     }
+
+    public String getCurrentUserName() {
+        return SecurityUtil.getSessionUser();
+    }
+
 }
