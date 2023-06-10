47fb40a10d0795ceb07e22d5dcbd336c7e9d010d
shehab kotb <shehabktob@gmail.com>
2023-06-10 15:43:14 +0300

    change cart button to go to user cart

---
+++ src/main/java/com/vodafone/ecommerce/security/CustomUserDetails.java
@@ -0,0 +1,19 @@
+package com.vodafone.ecommerce.security;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+@Getter
+public class CustomUserDetails extends User {
+    private Long id;
+
+    @Builder(builderMethodName = "CustomUserDetailsBuilder")
+    public CustomUserDetails(String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities, Long id) {
+        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+        this.id = id;
+    }
+}
