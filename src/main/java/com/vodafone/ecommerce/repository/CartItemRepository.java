7f2bd222b0444bda9840344aa46c6aade191eb98
Muhammad Bakr <mbakr7757@gmail.com>
2023-06-11 12:40:08 +0300

    add deleteByProductId & findAllByShoppingCartId methods

--- src/main/java/com/vodafone/ecommerce/repository/CartItemRepository.java
+++ src/main/java/com/vodafone/ecommerce/repository/CartItemRepository.java
@@ -4,7 +4,11 @@ import com.vodafone.ecommerce.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     CartItem findByProductId(Long productId);
+    List<CartItem> findAllByShoppingCartId(Long cartId);
+    void deleteByProductId(Long productId);
 }
