80b4a26fcafd230b620664016ba18612c5105001
voisDev <ibrahemkhaled1907@gmail.com>
2023-06-11 19:30:33 +0200

    Modify Payment Card Controller

--- src/main/java/com/vodafone/ecommerce/controller/PaymentCardController.java
+++ src/main/java/com/vodafone/ecommerce/controller/PaymentCardController.java
@@ -1,22 +1,11 @@
 package com.vodafone.ecommerce.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.vodafone.ecommerce.exception.InsufficientBalanceException;
-import com.vodafone.ecommerce.exception.NotFoundException;
+import com.vodafone.ecommerce.dto.PaymentDto;
 import com.vodafone.ecommerce.model.PaymentCard;
 import com.vodafone.ecommerce.service.PaymentCardService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 
-import javax.websocket.server.PathParam;
-import java.io.DataInput;
-import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -30,8 +19,8 @@ public class PaymentCardController {
 
     }
     @PostMapping(value = "/webapi/payments")
-    public String getCardDetails(@RequestBody PaymentCard paymentCard)  {
-        return paymentCardService.getCardDetails(paymentCard);
+    public String getCardDetails(@RequestBody PaymentDto paymentCard)  {
+        return paymentCardService.payFromPaymentCard(paymentCard);
     }
 
 }
