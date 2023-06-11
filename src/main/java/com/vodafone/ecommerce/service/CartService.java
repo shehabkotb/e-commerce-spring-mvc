9f974e36d3c91d3d46d22e8fe9153d5ca4105da0
Muhammad Bakr <mbakr7757@gmail.com>
2023-06-11 12:41:12 +0300

    add getUserCart & getCartTotalPrice & deleteProductFromCart methods to return the user cart and its total price

--- src/main/java/com/vodafone/ecommerce/service/CartService.java
+++ src/main/java/com/vodafone/ecommerce/service/CartService.java
@@ -1,8 +1,17 @@
 package com.vodafone.ecommerce.service;
 
+import com.vodafone.ecommerce.dto.CartItemDto;
 import com.vodafone.ecommerce.dto.ProductDto;
+import com.vodafone.ecommerce.model.CartItem;
+
+import java.util.List;
 
 public interface CartService {
     boolean isUserCartExist(Long userId);
     boolean addProductToCart(Long productId, Long quantity, Long userId);
+    List<CartItemDto> getUserCart(Long userId);
+    double getCartTotalPrice(Long userId);
+
+    void deleteProductFromCart(Long userId, Long productId);
+
 }
