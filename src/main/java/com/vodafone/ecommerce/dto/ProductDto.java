514008a42991498b1a616eac26a4ec625aedced7
shehab kotb <shehabktob@gmail.com>
2023-06-12 16:00:37 +0300

    fix for product add validation

--- src/main/java/com/vodafone/ecommerce/dto/ProductDto.java
+++ src/main/java/com/vodafone/ecommerce/dto/ProductDto.java
@@ -4,23 +4,23 @@ import com.vodafone.ecommerce.enums.Category;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @Builder
 public class ProductDto {
     private Long id;
     @NotBlank(message = "Product Should not be Empty")
-    @Max(value = 255)
+    @Size(max = 255)
     private String name;
     @NotBlank(message = "Photo Url Should not be Empty")
-    @Max(value = 255)
+    @Size(max = 255)
     private String photoUrl;
     @NotBlank(message = "Description Should not be Empty")
-    @Max(value = 255)
+    @Size(max = 255)
     private String description;
     @NotNull(message = "Price Should not be Empty")
     private Double price;
