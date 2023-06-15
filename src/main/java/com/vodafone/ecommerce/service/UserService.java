5f275c6b36f690058b12d972162269af063dd89c
Muhammad Bakr <mbakr7757@gmail.com>
2023-06-15 14:01:38 +0300

    add some functionalities and their implementation

--- src/main/java/com/vodafone/ecommerce/service/UserService.java
+++ src/main/java/com/vodafone/ecommerce/service/UserService.java
@@ -2,6 +2,7 @@ package com.vodafone.ecommerce.service;
 
 import com.vodafone.ecommerce.dto.RegistrationDto;
 import com.vodafone.ecommerce.dto.UserDto;
+import com.vodafone.ecommerce.model.UserEntity;
 
 import javax.mail.MessagingException;
 
@@ -12,6 +13,5 @@ public interface UserService {
     void resetAccount(String email) throws MessagingException;
 
     void confirmEmail(String confirmationToken);
-
-
+    UserEntity getUserById(Long userId);
 }
