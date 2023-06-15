caef6899da358c5b0370d64dec4b7d2ef0ff2011
voisDev <ibrahemkhaled1907@gmail.com>
2023-06-15 10:15:45 +0200

    unit test for totalPrice method

--- src/test/java/com/vodafone/ecommerce/service/CartItemServiceTest.java
+++ src/test/java/com/vodafone/ecommerce/service/CartItemServiceTest.java
@@ -1,6 +1,7 @@
 package com.vodafone.ecommerce.service;
 
 import com.vodafone.ecommerce.model.CartItem;
+import com.vodafone.ecommerce.model.Product;
 import com.vodafone.ecommerce.repository.CartItemRepository;
 import com.vodafone.ecommerce.repository.UserRepository;
 import com.vodafone.ecommerce.service.AdminService;
@@ -19,6 +20,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
@@ -47,5 +52,40 @@ import static org.mockito.Mockito.*;
         assertEquals(10L,cartItem.getQuantity());
 
     }
+    @Test
+    void calculateTotalPriceTest_calculateTotalPrice_returnTotalPrice(){
+        Product product1 = Product.builder()
+                .id(1L)
+                .name("Product1")
+                .price(20.0)
+                .build();
+        Product product2 = Product.builder()
+                .id(2L)
+                .name("Product2")
+                .price(20.0)
+                .build();
 
+        CartItem cartItem1=CartItem.builder()
+                .quantity(2L)
+                .product(product1)
+                .id(1L)
+                .build();
+        CartItem cartItem2=CartItem.builder()
+                .quantity(2L)
+                .product(product2)
+                .id(1L)
+                .build();
+
+        List<CartItem> cartItems = Arrays.asList(cartItem1, cartItem2);
+
+        when(cartItemRepository.findAllByShoppingCartId(cartItem1.getId())).thenReturn(cartItems);
+
+        double expectedTotalPrice = 80.0; // 2 x 20.0 + 2 x 20.0
+
+        double actualTotalPrice = cartItemSerivce.calculateTotalPrice(cartItem1.getId());
+
+        assertThat(actualTotalPrice).isEqualTo(expectedTotalPrice);
+
+
+    }
 }
